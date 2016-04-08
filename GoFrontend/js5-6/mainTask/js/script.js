8b44ec08d1f309c1ee4e3f0420e038be3c0cc911
Bondarenko Vlad <bondarenko.vladislavbondarenko@gmail.com>
2016-04-08 16:51:15 +0300

    js 5-6

--- GoFrontend/js5-6/mainTask/js/script.js
+++ GoFrontend/js5-6/mainTask/js/script.js
@@ -1,15 +1,14 @@
 var btnStart = document.querySelector('.button_start');
-var btnSplit = document.querySelector('.button_split');
-var btnReset = document.querySelector('.button_reset');
+var btnClear = document.querySelector('.button_clear');
 var display = document.querySelector('.display');
+var displayMs = document.querySelector('.display_ms');
 var ms = 0, ss = 0, mm = 0, hh = 0;
 var run = false;
 var startTime, contTime = 0;
 var timer;
 
 btnStart.addEventListener('click', start);
-btnReset.addEventListener('click', reset);
-btnSplit.addEventListener('click', split);
+btnClear.addEventListener('click', clear);
 
 function start() {
     if (run) {
@@ -19,6 +18,7 @@ function start() {
         btnStart.innerHTML = "Cont..";
         btnStart.classList.remove('button_pause');
     } else {
+        btnStart.innerHTML = "Cont..";
         startTime = Date.now();
         timer = setInterval(timerHandler,54);
         run = true;
@@ -39,24 +39,25 @@ function addZeroes(number, length) {
     return stringNum;
 }
 
-function format() {
+function formatMs() {
     ms = (Date.now() - startTime + contTime) % 1000;
+    return addZeroes(ms,3);
+}
+
+function format() {
     ss = (((Date.now() - startTime + contTime)/1000) % 60)>>0;
     mm = (((Date.now() - startTime + contTime)/60000) % 60)>>0;
     hh = ((Date.now() - startTime + contTime)/3600000)>>0;
-    return [addZeroes(hh,2)+':'+addZeroes(mm,2)+':'+addZeroes(ss,2)+'.'+addZeroes(ms,3)];
+    return [addZeroes(hh,2)+':'+addZeroes(mm,2)+':'+addZeroes(ss,2)];
 }
 
-function reset() {
+function clear() {
     clearInterval(timer);
-    display.innerHTML = '00:00:00.000';
+    display.innerHTML = '00:00:00';
+    displayMs.innerHTML = '000';
     ms = ss = mm = hh = 0; 
     run = false;
     contTime = 0;
     btnStart.innerHTML = "Start";
     btnStart.classList.remove('button_pause');
-}
-
-function split() {
-    
 }
