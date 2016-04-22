9f2b2ad699157c9b441593e53c88d72c5ca50d1c
Владислав Бондаренко < >
2016-04-22 18:17:00 +0300

    js9-10

--- GoFrontend/js9-10/js/script.js
+++ GoFrontend/js9-10/js/script.js
@@ -1,5 +1,7 @@
 $(function() {
     
+//CAROUSEL
+    
     $('.jcarousel').jcarousel({
     });
 
@@ -37,5 +39,48 @@ $(function() {
         return '<a href="#' + page + '">' + page + '</a>';
       }
     });
+    
+//MENU    
+    
+    $('.topLi').hover(
+        function() { $(this).children().slideDown(); },
+        function() { $(this).find('ul').slideUp('fast'); }
+    );
+    $('.innerLi').hover(
+	    function() { $(this).find('ul:first').slideDown(); },
+	    function() { $(this).find('ul:first').slideUp('fast'); }
+    );
+    
+    $('.topLi:has(ul)').find('a:first').append('<span class="arrow"> &#9660;</span>');
+    $('.innerLi:has(ul)').find('a:first').append('<span class="arrow arrow--inner"> &#9658;</span>');
+
+//CHECKBOXES
+    
+    $(".niceCheck").each(function() {
+         changeCheckStart($(this));
+    });
+    $(".niceCheck").mousedown(function() {
+         changeCheck(jQuery(this));
+    });
 
 });
+
+function changeCheck(el) {
+     var el = el,
+          input = el.find("input").eq(0);
+     if(!input.attr("checked")) {
+        el.css("background-position","0 -17px");    
+        input.attr("checked", true)
+    } else {
+        el.css("background-position","0 0");    
+        input.attr("checked", false)
+    }
+     return true;
+}
+function changeCheckStart(el) {
+    var el = el,
+    input = el.find("input").eq(0);
+    if(input.attr("checked"))
+        el.css("background-position","0 -17px");    
+     return true;
+}
